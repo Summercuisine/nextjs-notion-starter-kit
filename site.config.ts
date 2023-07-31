82c1073e06815a4e9cf706e085cd3249983f9b25
Lin feng <105482660+Summercuisine@users.noreply.github.com>
2023-07-31 17:03:11 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -26,7 +26,7 @@ export default siteConfig({
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
-  defaultPageIcon: 2.jpg,
+  defaultPageIcon: '/2.jpg',
   defaultPageCover: null,
   defaultPageCoverPosition: 0.5,
 
